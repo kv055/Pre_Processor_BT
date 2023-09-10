603fbedfdf758501bebb80ccbaf06eb0f6723dcd
Kilian Voss <kilian96@live.de>
2023-09-10 19:40:34 +1000

    complete rewrite, needs to be tested for data integrity

--- Data_pre_processor.py
+++ Data_pre_processor.py
@@ -1,22 +1,61 @@
+import pandas as pd
 
 class PreProcessor:
     def __init__(self):
         pass
         
-    def get_all_trades(self):
-        return self.back_trades_instance.return_all_trades()
+    def datastructure_VectorBT(self,asset_dicts):
+        asset_dicts_formated = []
+        for asset in asset_dicts:
+            asset_dicts_formated.append(self.price_and_trades_frame(asset))
+
+        return asset_dicts_formated
     
-    def get_trades_for_asset(self, asset):
-        return self.back_trades_instance.return_trades_for_asset(asset)
-    
-    def get_all_live_logs(self):
-        return self.live_logs_instance.return_all_log_strategy_output()
-    
-    def get_all_assets(self):
-        return self.price_data_instance.return_all_assets()
-    
-    def get_historical_ohlc(self, sample_dict):
-        return self.price_data_instance.return_historical_ohlc_from_db(sample_dict)
+    def price_and_trades_frame(self, asset):
+        # Prepare Pricedataframe
+        ohlc_data_frame = pd.DataFrame(asset['OHLC']) 
+
+        # ohlc_data_frame.set_index("TimeStamp", inplace = True)
+        ohlc_data_frame['Open'] = pd.to_numeric(ohlc_data_frame['Open'])
+        ohlc_data_frame['High'] = pd.to_numeric(ohlc_data_frame['High'])
+        ohlc_data_frame['Low'] = pd.to_numeric(ohlc_data_frame['Low'])
+        ohlc_data_frame['Close'] = pd.to_numeric(ohlc_data_frame["Close"])
+
+        del ohlc_data_frame['Open']
+        del ohlc_data_frame['High']
+        del ohlc_data_frame['Low']
+        del ohlc_data_frame['Average']
+        del ohlc_data_frame['DataProvider']
+        del ohlc_data_frame['CandleSize']
+        
+        # Convert your list of dictionaries into a DataFrame
+        data_frame = pd.DataFrame(asset['Trades'])
+
+        # Create a new column 'Entry' in data_frame, set to True
+        data_frame['Entry'] = True
+
+        # Merge the two DataFrames on the 'Timestamp'/'Open' columns
+        merged_df = pd.merge(ohlc_data_frame, data_frame[['Open', 'Entry']], left_on='Timestamp', right_on='Open', how='left')
+
+        # Drop the 'Open' column
+        merged_df.drop('Open', axis=1, inplace=True)
+
+        # Replace NaN values in 'Entry' column with False
+        merged_df['Entry'].fillna(False, inplace=True)
+
+        # Create a new column 'Exits' in data_frame, set to True
+        data_frame['Exits'] = True
+
+        # Merge the 'Exits' column into the existing DataFrame
+        merged_df = pd.merge(merged_df, data_frame[['Closed', 'Exits']], left_on='Timestamp', right_on='Closed', how='left')
+
+        # Drop the 'Closed' column
+        merged_df.drop('Closed', axis=1, inplace=True)
+
+        # Replace NaN values in 'Exits' column with False
+        merged_df['Exits'].fillna(False, inplace=True)
+
+        print(merged_df)
 
 
-
+        return ohlc_data_frame
