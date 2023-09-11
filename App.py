cdce35b6571ae27f7f904661aa99aa5e46ca5e16
Kilian Voss <kilian96@live.de>
2023-09-11 15:30:16 +1000

    deleted old code, first draft that takes all alreday formated data and runs a simple backtest

--- App.py
+++ App.py
@@ -1,26 +1,19 @@
 from Data_pre_processor import PreProcessor
-from vectorbt_master import vbt
+from vectorbt_master import vectorbt as vbt
 
 
 def Backtrading(test_trades):
     Test_data_instance = PreProcessor()
-    test_ohlc_frame = Test_data_instance.datastructure_VectorBT(test_trades)
+    # Formating and appending a pandas dataframe tailored to vectorBT
+    Test_data_instance.datastructure_VectorBT(test_trades)
     
-    for position in test_trades['trades']:
-        position_open = position['Open'].strftime("%Y-%m-%d")
-        position_close = position['Closed'].strftime("%Y-%m-%d")
+    for asset in test_trades:
+        vbt_frame = asset['VBT_Frame']
+        closing_price = vbt_frame['Close']
+        entries = vbt_frame['Entry']
+        exits = vbt_frame['Exits']
 
-        test_ohlc_frame.loc[position_open, 'Entries'] = True
-        test_ohlc_frame.loc[position_close, 'Exits'] = True
-
-    test_ohlc_frame.fillna(False, inplace=True)
-
-    closing_price = test_ohlc_frame['Close']
-    entries = test_ohlc_frame['Entries']
-    exits = test_ohlc_frame['Exits']
-
-
-    pf = vbt.Portfolio.from_signals(closing_price, entries, exits, init_cash=100)
-    profit = pf.total_profit()
-
-    l = 0
+        pf = vbt.Portfolio.from_signals(closing_price, entries, exits, init_cash=100)
+        profit = pf.total_profit()
+        print(profit)
+        asset['VBT_PnL'] = profit
